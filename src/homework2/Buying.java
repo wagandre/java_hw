1cb93f10d386301340c0003f16cd74607df4d38e
wagandre <wagnerandro01@gmail.com>
2023-02-20 19:05:05 +0100

    homework #2

--- src/homework2/Buying.java
+++ src/homework2/Buying.java
@@ -4,22 +4,28 @@ public class Buying {
     public static void main(String[] args) {
 
 
-        boolean isEdekaOpen = true;
+        boolean isEdekaOpen = false;
         boolean isReweOpen = false;
-        boolean canBuy = false;
-        checkingWhereIsOpen(isEdekaOpen,isReweOpen,canBuy);
+
+        checkingWhereIsOpen(isEdekaOpen,isReweOpen);
     }
 
-        public static void checkingWhereIsOpen(boolean isEdekaOpen,boolean isReweOpen,  boolean canBuy ) {
+        public static void checkingWhereIsOpen(boolean isEdekaOpen,boolean isReweOpen) {
+        String store;
 
             if (isEdekaOpen || isReweOpen) {
-                canBuy = true;
-                System.out.println("Я могу купить еду, это " + canBuy);
-            } else if (!isEdekaOpen && !isReweOpen) {
-                canBuy = false;
-                System.out.println("Я могу купить еду, это " + canBuy);
+                if(isEdekaOpen &&!isReweOpen){
+                    store = "Edeka";}
+                else if(!isEdekaOpen && isReweOpen)
+                    store = "Rewe";
+                else store = "Edeka and Rewe";
+                System.out.println("I can go to  " + store);
+            }
+           else
+
+                System.out.println("All stores are closed");
             }
 
 
         }
-    }
+
